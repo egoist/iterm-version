4fcd8e0b5e1dbdebcbd253259534b19f56d41ced
EGOIST <0x142857@gmail.com>
2016-03-11 19:19:49 +0800

    use system promise

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ test('main', async t => {
 	try {
 		let version = await v()
 		version = version.split('.')
+		console.log(version[0] > 1)
 		t.true(version[0] > 1)
 		t.true(version[1] > 8)
 	} catch (e) {
